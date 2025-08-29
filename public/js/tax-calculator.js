2b5a104e7030ca001b267d7fa82d2bf91837cccf
taxeasyza-oss <taxeasyza@gmail.com>
2025-08-29 11:05:35 +0000

    #task 37
    Signed-off-by: taxeasyza-oss <taxeasyza@gmail.com>

--- public/js/tax-calculator.js
+++ public/js/tax-calculator.js
@@ -4,10 +4,10 @@ class TaxCalculator {
       { min: 0, max: 237100, rate: 0.18, base: 0 },
       { min: 237101, max: 370500, rate: 0.26, base: 42678 },
       { min: 370501, max: 512800, rate: 0.31, base: 77362 },
-      { min: 512801, max: 673000, rate: 0.36, base: 121475 },
+      { min: 512801, max: 673000, rate: 0.36, base: 121476 },
       { min: 673001, max: 857900, rate: 0.39, base: 179147 },
       { min: 857901, max: 1817000, rate: 0.41, base: 251258 },
-      { min: 1817001, max: Infinity, rate: 0.45, base: 644489 }
+      { min: 1817001, max: Infinity, rate: 0.45, base: 644490 }
     ];
     this.rebates = { primary: 17235, secondary: 9444, tertiary: 3145 };
     this.bindEvents();
