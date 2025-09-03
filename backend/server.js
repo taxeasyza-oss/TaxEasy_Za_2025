091ef86d2589d46e3506104f5ca13243fcd42832
taxeasyza-oss <taxeasyza@gmail.com>
2025-09-03 13:24:32 +0000

    fix: remove unused payfast require

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-const { processPayment } = require('./payfast-service');
 const logger = require('./logger');
 
 const app = express();
